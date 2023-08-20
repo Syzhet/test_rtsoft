ec6d26c00f7ee381df4a560af0d45f46e3ce2b9e
Алексей <click742@yandex.ru>
2023-08-20 18:25:46 +0300

    В файле ../app/main.py добавлен обработчик исключений. В файле ../db/schemas/schemas.py добавлена валидация количества параметров запроса. В файле ../templates/response.html добавлена обработка если изображений не найдено.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
+from fastapi.responses import PlainTextResponse
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 from app.routers import router
 from config.config import base_config
@@ -10,6 +11,17 @@ app.include_router(
     router=router,
     prefix='/api/v1'
 )
+
+
+@app.exception_handler(ValueError)
+async def validation_exception_handler(
+    request, exc: ValueError
+) -> PlainTextResponse:
+    """Обработка исключений при валидации передаваемых параметров запроса."""
+    return PlainTextResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content=str(exc).split('\n')[2]
+    )
 
 if __name__ == '__main__':
     uvicorn.run(
--- db/schemas/schemas.py
+++ db/schemas/schemas.py
@@ -1,7 +1,7 @@
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from fastapi import Query
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
 class QueryParametr(BaseModel):
@@ -10,3 +10,13 @@ class QueryParametr(BaseModel):
     category: Optional[List[str]] = Field(
         Query([], alias="category[]")
     )
+
+    @validator('category')
+    def check_query_category(cls, value) -> Union[List[str], Exception]:
+        """Проверка количества передаваемых категорий."""
+
+        if len(value) <= 10:
+            return value
+        raise ValueError(
+            'Нельзя передавать больше 10 категорий в запросе'
+        )
